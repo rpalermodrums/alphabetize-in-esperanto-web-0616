b5931e890067fbdcb7a525d03ce70115c6b282dc
Ryan Palermo <stix250@gmail.com>
2016-05-21 05:34:59 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
 def alphabetize(arr)
   # code here
+  dict = {}
+  alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  alph.each_with_index do |letter, i|
+    dict[letter] = i
+  end
+  arr.sort do |word, other_word|
+    i = 0
+    while word[i] == other_word[i] do
+      i += 1
+    end
+    dict[word[i]] <=> dict[other_word[i]]
+  end
 end
